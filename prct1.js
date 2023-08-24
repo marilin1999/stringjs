1ca7ee1b3a75be9aa34f7a877ad5c8400ef0e5f4
marilin1999 <140349328+marilin1999@users.noreply.github.com>
2023-08-24 17:56:35 -0500

    Add files via upload

---
+++ prct1.js
@@ -0,0 +1,14 @@
+let miRefran = "mas vale tarde que nunca";
+
+let longitudRefran = (miRefran.length);
+let novenaletra = (miRefran.charAt(9));
+let ultimaLetra = (miRefran.length-1);
+let indicedePalabra = (miRefran.indexOf("que"));
+
+
+//console.log(miRefran)
+console.log(longitudRefran)
+console.log(novenaletra)
+console.log(ultimaLetra)
+console.log(miRefran.replace("que","(palabrarobada)"));
+console.log(miRefran.toLocaleUpperCase());
